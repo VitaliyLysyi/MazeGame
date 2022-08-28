bff705c7a68187804bf80dfe17b71bac9b88be32
VitaliyLysyi <85xq596ijavc7da>
2022-08-28 14:17:34 +0300

    Create maze prefabs
    Rings and Floor brefabs

---
+++ Assets/_project/CodeBase/Maze/MazeRing.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace CodeBase
+{
+    public class MazeRing : MonoBehaviour, IMovable
+    {
+        private const float ROTATION_SPEED = 25f;
+
+        public void move(float horizontal, float vertical)
+        {
+            Vector3 rotation = horizontal * ROTATION_SPEED * Time.deltaTime * Vector3.up;
+            transform.Rotate(rotation);
+        }
+    }
+}
